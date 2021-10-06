c8e97d4409647b975bc7edb6222dce06acfb5aef
HuuThang-1402 <thang.nguyen2018@hcmut.edu.vn>
2021-10-06 19:51:22 +0700

    Update file

--- src.js
+++ src.js
@@ -1,11 +1,11 @@
-var myCenter = new google.maps.LatLng(10.7738297, 106.7036873);
+var myCenter = new google.maps.LatLng(10.87846, 106.80664);
 
 function initialize() {
 var mapProp = {
   center:myCenter,
   zoom:12,
-  scrollwheel:false,
-  draggable:false,
+  scrollwheel:true,
+  draggable:true,
   mapTypeId:google.maps.MapTypeId.ROADMAP
 };
 
@@ -19,3 +19,29 @@ marker.setMap(map);
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
+
+
+// $(document).ready(function(){
+//   $(window).scroll(function(){
+//     $(".slideanim").each(function(){
+//       var pos = $(this).offset().top;
+//       var winTop = $(window).scrollTop();
+//       if (pos< winTop+600){
+//         $(this).addClass("slide");
+//       }
+//     });
+//   });
+// });
+
+$(document).ready(function () {
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 30,
+    nav: true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    items: 3
+  });
+
+});
